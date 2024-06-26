2987d21a35f640689d785d913cef556e85bede0e
Vladislav Glotov <vbiohazard10@gmail.com>
2024-06-26 10:52:24 +0300

    HW02.8 - add authentication

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -11,11 +11,23 @@ import java.io.IOException;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserServlet extends HttpServlet {
+    private final static String USER_ID = "userId";
+    private final static String USERS_JSP = "/users.jsp";
+    private final static String MEALS = "meals";
+
     private static final Logger log = getLogger(UserServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int userId = Integer.valueOf(request.getParameter(USER_ID));
+        SecurityUtil.setAuthUserId(userId);
+        response.sendRedirect(MEALS);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to users");
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        request.getRequestDispatcher(USERS_JSP).forward(request, response);
+        response.sendRedirect(USERS_JSP);
     }
 }
