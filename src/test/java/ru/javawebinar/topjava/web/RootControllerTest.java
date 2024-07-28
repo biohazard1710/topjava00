f6862973c199d12515ff991f8f9b8a974bd212dd
Vladislav Glotov <vbiohazard10@gmail.com>
2024-07-28 10:38:55 +0300

    HW07.1.1 - add getMeals method for RootControllerTes

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
@@ -10,17 +11,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
 
+    private static final String USERS_URL_TEMPLATE = "/users";
+    private static final String MEALS_URL_TEMPLATE = "/meals";
+    private static final String USERS_EXPECTED_VIEW = "users";
+    private static final String MEALS_EXPECTED_VIEW = "meals";
+    private static final String USERS_JSP_FORWARDER_URL = "/WEB-INF/jsp/users.jsp";
+    private static final String MEALS_JSP_FORWARDER_URL = "/WEB-INF/jsp/meals.jsp";
+    private static final String USERS_MODEL_ATTRIBUTE = "users";
+    private static final String MEALS_MODEL_ATTRIBUTE = "meals";
+
     @Test
     void getUsers() throws Exception {
-        perform(get("/users"))
+        perform(get(USERS_URL_TEMPLATE))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(view().name("users"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
-                .andExpect(model().attribute("users",
+                .andExpect(view().name(USERS_EXPECTED_VIEW))
+                .andExpect(forwardedUrl(USERS_JSP_FORWARDER_URL))
+                .andExpect(model().attribute(USERS_MODEL_ATTRIBUTE,
                         new AssertionMatcher<List<User>>() {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
@@ -29,4 +40,22 @@ class RootControllerTest extends AbstractControllerTest {
                         }
                 ));
     }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get(MEALS_URL_TEMPLATE))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name(MEALS_EXPECTED_VIEW))
+                .andExpect(forwardedUrl(MEALS_JSP_FORWARDER_URL))
+                .andExpect(model().attribute(MEALS_MODEL_ATTRIBUTE,
+                        new AssertionMatcher<List<Meal>>() {
+                            @Override
+                            public void assertion(List<Meal> actual) throws AssertionError {
+                                MEAL_MATCHER.assertMatch(actual, meals);
+                            }
+                        }
+                ));
+    }
+
 }
